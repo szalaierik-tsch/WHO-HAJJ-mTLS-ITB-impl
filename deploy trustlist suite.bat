4175f2380ab69a2f57d2ab45b8c35b86e7fa08bc
Erik Szalai <erik.szalai@t-systems.com>
2025-08-21 11:13:52 +0200

    Split up specifications. Using DOMAIN property for helper service so that it uses the internal helper service, not the one on my machine. Cleaned up actors and their descriptions. Cleaned up test case description texts.

--- deploy trustlist suite.bat
+++ deploy trustlist suite.bat
@@ -1,4 +1,4 @@
 echo Deploying test suite 3...
 rm -f ts-st-participant-trustlist.zip
 7z a ts-st-participant-trustlist.zip .\ts-st-participant-trustlist\*
-curl -F updateSpecification=true -F specification=513CD9B7XE8EDX4508X84EEX320C8F9022B5 -F testSuite=@ts-st-participant-trustlist.zip --header "ITB_API_KEY: 2E86828DXEDB9X4C5CX8D5DX5BF0A406DAB9" -X POST http://localhost:9003/api/rest/testsuite/deploy
+curl -F updateSpecification=true -F specification=2B7CAD26X5AC6X40E8X9D73X4CA08073513A -F testSuite=@ts-st-participant-trustlist.zip --header "ITB_API_KEY: 2E86828DXEDB9X4C5CX8D5DX5BF0A406DAB9" -X POST http://localhost:9003/api/rest/testsuite/deploy
--- deploy vhl suite.bat
+++ deploy vhl suite.bat
@@ -1,4 +1,4 @@
 echo Deploying test suite 3...
 rm -f ts-st-participant-vhl.zip
 7z a ts-st-participant-vhl.zip .\ts-st-participant-vhl\*
-curl -F updateSpecification=true -F specification=513CD9B7XE8EDX4508X84EEX320C8F9022B5 -F testSuite=@ts-st-participant-vhl.zip --header "ITB_API_KEY: 2E86828DXEDB9X4C5CX8D5DX5BF0A406DAB9" -X POST http://localhost:9003/api/rest/testsuite/deploy
+curl -F updateSpecification=true -F specification=9E4C3D39XEB9CX4A5EX8D58XD8E8338E171D -F testSuite=@ts-st-participant-vhl.zip --header "ITB_API_KEY: 2E86828DXEDB9X4C5CX8D5DX5BF0A406DAB9" -X POST http://localhost:9003/api/rest/testsuite/deploy
